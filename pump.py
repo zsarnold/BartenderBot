0c7836591aae87a8ebaba69bcbc3f0d51ce812f1
zsarnold <zsarnold@mtu.edu>
2020-12-29 14:56:39 -0500

    i2c adr info

--- pump.py
+++ pump.py
@@ -2,15 +2,28 @@
 #https://github.com/zsarnold
 #Pump file. Used to control pumps for certain time.
 
-#import smbus
-#import time
+import smbus
+import time
 
-#pin out for each pump
+#I2C setup
+bus = smbus.SMBus(1)
+
+DEVICE = 0x20
+IODIRB = 0x01
+OLATB  = 0x15
+GPIOB  = 0x13
+
+#Set GPA pins to output
+bus.write_byte_data(DEVICE, IODIRB, 0x00)
+
+#Set all ports to 0
+bus.write_byte_data(DEVICE, OLATB, 0)
 
 
 def pump( pumpNum, time ):
     #set pump high
-
+    bus.write_byte_data(DEVICE, OLATB, pumpNum)
     #wait the time
-
+    time.sleep(time)
     #set pump low
+    bus.write_byte_data(DEVICE, OLATB, 0)
--- step.py
+++ step.py
@@ -2,8 +2,22 @@
 #https://github.com/zsarnold
 #stepper file. Used to control the stepper motor
 
-#import smbus
-#import time
+import smbus
+import time
+
+#I2C setup
+bus = smbus.SMBus(1)
+
+DEVICE = 0x20
+IODIRA = 0x00
+OLATA  = 0x14
+GPIOA  = 0x12
+
+#Set GPA pins to output
+bus.write_byte_data(DEVICE, IODIRA, 0x00)
+
+#Set all ports to 0
+bus.write_byte_data(DEVICE, OLATA, 0)
 
 #pin assignments
 A1 =
